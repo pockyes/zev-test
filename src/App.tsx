f4f6a832f225f494f2df60e5ab3fecde0e2207ea
[pockyes] <[tele1841@gmail.com]>
2022-08-20 15:57:01 +0900

    zev 시작...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
+const App: React.FC = () => {
+  useEffect(() => {
+    let container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
+    let option =  {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3 //지도의 레벨(확대, 축소 정도)
+    };
+
+    let map = new window.kakao.maps.Map(container, option); // 지도 생성 및 객체 리턴
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="map" style={{ width: "100vw", height: "100vh" }} />
     </div>
   );
 }
